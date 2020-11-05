10b391e1eda3fdd127a3d85bffae780edfb5a833
alisaid-sm <alisaid7170@gmail.com>
2020-11-06 00:43:58 +0700

    Revert "all done"
    This reverts commit a1841d3f6b92d3ce73ee813c0547b82a7433f4df.

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux'
 
 import country from './country'
-import corona from './corona'
 
 const reducers = combineReducers({
-    country,
-    corona
+    country
 })
 
 export default reducers
