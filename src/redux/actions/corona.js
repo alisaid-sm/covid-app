0ffa48df602eea79322ea0ffe4d6f04504f2efe9
alisaid-sm <alisaid7170@gmail.com>
2020-11-06 00:48:10 +0700

    add select by country

--- src/redux/actions/corona.js
+++ src/redux/actions/corona.js
@@ -1,10 +1,11 @@
 import axios from 'axios'
 
-export const GET_DETAIL_CORONA = () => {
+export const GET_DETAIL_CORONA = (val) => {
+    const country = val === undefined? '' : val
     return {
         type: 'GET_DETAIL_CORONA',
         payload: new Promise ((resolve, reject) => {
-            axios.get(`https://api.covid19api.com/total/country/indonesia`)
+            axios.get(`https://api.covid19api.com/total/country/${country}`)
                 .then((response) => {
                     resolve(response.data)
                 }).catch((err) => {
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,24 +1,31 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 
 import { useSelector, useDispatch } from 'react-redux'
 
 import BG from "../assets/img/head 2.png"
 
 import { GET_ALL_COUNTRIES } from '../redux/actions/country'
+import { GET_DETAIL_CORONA } from '../redux/actions/corona'
 
 export default function Home() {
+  let [countrySelect, setCountrySelect] = useState('')
   const dispatch = useDispatch()
   const country = useSelector((state) => state.country)
-<<<<<<< HEAD
   const getCorona = useSelector((state) => state.corona)
-  // console.log(getCorona)
-  const corona = getCorona.list[getCorona.list.length - 1]
-
-=======
-  
->>>>>>> parent of a1841d3... all done
+  const coronaFilter = getCorona.list[getCorona.list.length - 1] 
+  const { Confirmed, Recovered, Deaths, Date } = coronaFilter === undefined? { Confirmed: 0, Recovered: 0, Deaths: 0, Date: '' } : coronaFilter
+  const kasus = Confirmed.toString().split('').reverse().join('').match(/\d{1,3}/g).join('.').split('').reverse().join('')
+  const sembuh = Recovered.toString().split('').reverse().join('').match(/\d{1,3}/g).join('.').split('').reverse().join('')
+  const mati = Deaths.toString().split('').reverse().join('').match(/\d{1,3}/g).join('.').split('').reverse().join('')
+  const tanggal = Date.substr(0, 10)
+  // console.log({kasus, sembuh, mati, tanggal})
+  const onCountrySelect = (value) => {
+    setCountrySelect(countrySelect = value)
+    dispatch(GET_DETAIL_CORONA(countrySelect))
+  }
   useEffect(() => {
     dispatch(GET_ALL_COUNTRIES())
+    dispatch(GET_DETAIL_CORONA())
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
   return (
@@ -27,15 +34,11 @@ export default function Home() {
             <img src={BG} alt="BG" />
         </div>
         <div className="date-new">
-            <p>Terakhir Update : 01 Oktober 2020 </p>
+            <p>Terakhir Update : { tanggal } </p>
         </div>
         <div className="form-group select-custom">
-<<<<<<< HEAD
-            <select className="form-control" >
-=======
-            <select className="form-control">
->>>>>>> parent of a1841d3... all done
-                <option defaultValue>Negara</option>
+            <select className="form-control" value={ countrySelect } onChange={(val) => onCountrySelect(val.target.value)}>
+                <option value={''} defaultValue>Negara</option>
                 {
                   country.list.map((item, index) => (
                     <option key={index} value={item.Slug} >{item.Country}</option>
@@ -45,43 +48,30 @@ export default function Home() {
         </div>
         <div className="row">
             <div className="col-lg-4 col-md-6">
-              <div className="card mb-md-3" style={{borderBottom: "solid 21.63px #7F90FC"}}>
+              <div className="card" style={{borderBottom: "solid 21.63px #7F90FC"}}>
                 <div className="card-body">
-<<<<<<< HEAD
-                  <p className="card-title">kasus</p>
-                  <p className="card-number">{ corona.Confirmed }</p>
-=======
                   <p className="card-title">Kasus</p>
-                  <p className="card-number">10,000,000</p>
->>>>>>> parent of a1841d3... all done
+                  <p className="card-number">{ kasus }</p>
                   <p className="card-title">Orang</p>
                   <p className="card-text">Jumlah angka kasus COVID - 19</p>
                 </div>
               </div>
             </div>
             <div className="col-lg-4 col-md-6">
-              <div className="card mb-md-3" style={{borderBottom: "solid 21.63px #90FE99"}}>
+              <div className="card" style={{borderBottom: "solid 21.63px #90FE99"}}>
                 <div className="card-body">
                     <p className="card-title" >Sembuh</p>
-<<<<<<< HEAD
-                    <p className="card-number">{ corona.Recovered }</p>
-=======
-                    <p className="card-number">10,000,000</p>
->>>>>>> parent of a1841d3... all done
+                    <p className="card-number">{ sembuh }</p>
                     <p className="card-title">Orang</p>
                     <p className="card-text">Jumlah angka sembuh COVID - 19</p>
                 </div>
               </div>
             </div>
             <div className="col-lg-4 col-md-6">
-                <div className="card mb-md-3" style={{borderBottom: "solid 21.63px #FE7F89"}}>
+                <div className="card" style={{borderBottom: "solid 21.63px #FE7F89"}}>
                   <div className="card-body">
                     <p className="card-title" >Meninggal</p>
-<<<<<<< HEAD
-                    <p className="card-number">{ corona.Deaths }</p>
-=======
-                    <p className="card-number">10,000,000</p>
->>>>>>> parent of a1841d3... all done
+                    <p className="card-number">{ mati }</p>
                     <p className="card-title">Orang</p>
                     <p className="card-text">Jumlah angka meninggal COVID - 19</p>
                   </div>
